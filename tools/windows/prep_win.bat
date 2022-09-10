787f29e1362988d5bdc73beded69f052a13bddfe
Ak <44728139+Ak-wa@users.noreply.github.com>
2022-09-10 15:30:39 +0200

    Create prep_win.bat

---
+++ tools/windows/prep_win.bat
@@ -0,0 +1,6 @@
+:: Extracts zip & rar files from folder (github detects them as malware so it has to be done this way)
+unrar x -r mimikatz_trunk.rar
+unzip netcat-win32-1.12.zip -d netcat
+wget https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip
+unzip netcat-win32-1.11.zip
+mv netcat-1.11/ netcat/
