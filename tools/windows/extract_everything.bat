1164450057ac0dbc9e3310de80442be86749a01a
Ak <44728139+Ak-wa@users.noreply.github.com>
2022-09-10 15:57:59 +0200

    Update extract_everything.bat

--- tools/windows/extract_everything.bat
+++ tools/windows/extract_everything.bat
@@ -4,3 +4,4 @@ unzip netcat-win32-1.12.zip -d netcat
 wget https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip
 unzip netcat-win32-1.11.zip
 mv netcat-1.11/ netcat/
+git clone https://github.com/SecWiki/windows-kernel-exploits
